79665c5ad787cef68917e5f8b71d8a6750c66899
IyadAbed <iyadrabed@gmail.com>
2023-05-16 09:59:10 +0300

    push

--- client/src/body.jsx
+++ client/src/body.jsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const MyComponent = () => {
-  const [movie, setMovie] = useState(null);
+  const [movie, setMovie] = useState([]);
 
   useEffect(() => {
     // Make the HTTP request to the server when the component mounts
     axios
-      .get("http://localhost:5000")
+      .get("http://localhost:5000/movies")
       .then((response) => {
         // Handle the response data
         setMovie(response.data);
@@ -17,43 +17,19 @@ const MyComponent = () => {
         console.error(error);
       });
   }, []);
-
+ console.log(movie)
   return (
-    <>
-      <br />
-      <br />
-      <br />
-      <div className="container mx-auto py-8">
-        <div className="max-w-lg mx-auto bg-white rounded-lg shadow-lg">
-          <div className="p-6">
-            {movie ? (
-              <div className="movie-card">
-                <img
-                  src={movie.poster_path}
-                  alt={movie.title}
-                  className="mb-4 rounded"
-                />
-                <h2 className="text-2xl font-bold mb-2">{movie.title}</h2>
-                <p className="text-gray-600">{movie.overview}</p>
-                <p className="text-gray-600">
-                  Release Date: {movie.release_date}
-                </p>
-                <p className="text-gray-600">
-                  Genres: {movie.genres.map((genre) => genre.name).join(", ")}
-                </p>
-                <p className="text-gray-600">
-                  Runtime: {movie.runtime} minutes
-                </p>
-                <p className="text-gray-600">Rating: {movie.vote_average}</p>
-                <p className="text-gray-600">Revenue: ${movie.revenue}</p>
-              </div>
-            ) : (
-              <p>Loading...</p>
-            )}
-          </div>
-        </div>
+    <div className="bg-gray-900 text-white p-4">
+    {movie.map((movie) => (
+      <div key={movie.id} className="border-b border-gray-700 pb-4 mb-4">
+        <h2 className="text-2xl font-bold mb-2">{movie.title}</h2>
+        <p className="text-gray-400">{movie.description}</p>
+        <img className="mt-4" src={movie.thumbnail} alt={movie.title} />
+        {/* Add more JSX to display other movie details */}
       </div>
-    </>
+    ))}
+  </div>
+  
   );
 };
 
--- server/node.js
+++ server/node.js
@@ -3,38 +3,30 @@ const cors = require("cors");
 const { default: axios } = require("axios");
 
 const app = express();
+app.use(express.json());
+
 const port = 5000; // Choose any available port
 app.use(cors());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+const options = {
+  method: 'GET',
+  url: 'https://imdb-top-100-movies1.p.rapidapi.com/',
+  headers: {
+    'X-RapidAPI-Key': '551612c944msh6e61933e9213b7ep175b8ajsn5aa6407b6575',
+    'X-RapidAPI-Host': 'imdb-top-100-movies1.p.rapidapi.com'
+  }
+};
 
 // Define your server endpoints
-app.get("/", (req, res) => {
-  // Make the HTTP request to the API
-  const apiUrl = "https://api.themoviedb.org/3/movie/550";
-  const apiKey = "f7c60082eff916bf6b71057813e68f3c";
-
+app.get("/movies", (req, res) => {
   axios
-    .get(apiUrl, {
-      params: {
-        api_key: apiKey,
-      },
-    })
-    .then((response) => {
-      // Handle the API response data
-      const data = response.data;
-      res.json(data);
-    })
-    .catch((error) => {
-      // Handle any errors
-      console.error(error);
-      res
-        .status(500)
-        .json({ error: "An error occurred while fetching data from the API." });
-    });
+  .request(options)
+  .then((response) => {
+    res.json(response.data);
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
+  });
 });
 // Start the server
 app.listen(port, () => {
